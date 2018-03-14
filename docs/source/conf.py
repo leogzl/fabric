f94ce808251dd5c05a454338bf36b38b17eff223
Leonardus Gazali <leogzl@gmail.com>
2018-03-14 08:28:33 +0000

    change the author to PT

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -152,7 +152,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'BLOCKCHAIN TRANSACTION SERVICE.tex', u'BLOCKCHAIN TRANSACTION SERVICE (HYPERLEDGER FABRIC)',
-     u'TOYOTA MOTOR MANUFACTORING INDONESIA', 'manual'),
+     u'PT. TOYOTA MOTOR MANUFACTURING INDONESIA', 'manual'),
 ]
 
 
